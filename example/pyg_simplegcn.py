905c17fb8114477fa1816aad48898d7bd120d9c2
jh0shin <jh0shin2004@naver.com>
2021-09-08 13:04:51 +0900

    Modify tracking python package log

--- example/pyg_simplegcn.py
+++ example/pyg_simplegcn.py
@@ -8,57 +8,39 @@ import torch.nn.functional as F
 from torch_geometric.nn import GCNConv
 from torch_geometric.datasets import Planetoid
 
-import sys
-import trace
+dataset = Planetoid(root='/tmp/Cora', name='Cora')
 
-def main():
-    dataset = Planetoid(root='/tmp/Cora', name='Cora')
+class Net(torch.nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = GCNConv(dataset.num_node_features, 16)
+        self.conv2 = GCNConv(16, dataset.num_classes)
 
-    class Net(torch.nn.Module):
-        def __init__(self):
-            super(Net, self).__init__()
-            self.conv1 = GCNConv(dataset.num_node_features, 16)
-            self.conv2 = GCNConv(16, dataset.num_classes)
+    def forward(self, data):
+        x, edge_index = data.x, data.edge_index
 
-        def forward(self, data):
-            x, edge_index = data.x, data.edge_index
+        x = self.conv1(x, edge_index)
+        x = F.relu(x)
+        x = F.dropout(x, training=self.training)
+        x = self.conv2(x, edge_index)
 
-            x = self.conv1(x, edge_index)
-            x = F.relu(x)
-            x = F.dropout(x, training=self.training)
-            x = self.conv2(x, edge_index)
+        return F.log_softmax(x, dim=1)
 
-            return F.log_softmax(x, dim=1)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = Net().to(device)
+data = dataset[0].to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
 
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model = Net().to(device)
-    data = dataset[0].to(device)
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
+model.train()
+for epoch in range(1):
+    optimizer.zero_grad()
+    out = model(data)
+    loss = F.nll_loss(out[data.train_mask], data.y[data.train_mask])
+    loss.backward()
+    optimizer.step()
 
-    model.train()
-    for epoch in range(1):
-        optimizer.zero_grad()
-        out = model(data)
-        loss = F.nll_loss(out[data.train_mask], data.y[data.train_mask])
-        loss.backward()
-        optimizer.step()
-
-    model.eval()
-    _, pred = model(data).max(dim=1)
-    correct = float (pred[data.test_mask].eq(data.y[data.test_mask]).sum().item())
-    acc = correct / data.test_mask.sum().item()
-    print('Accuracy: {:.4f}'.format(acc))
-
-# create a Trace object, telling it what to ignore, and whether to
-# do tracing or line-counting or both.
-tracer = trace.Trace(
-    ignoredirs=[sys.prefix, sys.exec_prefix],
-    trace=0,
-    count=1)
-
-# run the new command using the given tracer
-tracer.run('main()')
-
-# make a report, placing output in the current directory
-r = tracer.results()
-r.write_results(show_missing=True, coverdir="./trace_pyg")
+model.eval()
+_, pred = model(data).max(dim=1)
+correct = float (pred[data.test_mask].eq(data.y[data.test_mask]).sum().item())
+acc = correct / data.test_mask.sum().item()
+print('Accuracy: {:.4f}'.format(acc))
